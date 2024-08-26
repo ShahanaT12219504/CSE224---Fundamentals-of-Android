e8a84354e249a6c8a6221e416c71745a04765bdc
Shahana T_12219504 <119865026+ShahanaT12219504@users.noreply.github.com>
2024-08-26 13:33:13 +0530

    Create Control flow1.kt

---
+++ Control flow1.kt
@@ -0,0 +1,17 @@
+fun main() {
+    val grade = 85
+    if(grade >= 90){
+        println("Grade A")
+    }
+    else if(grade >= 80){
+        println("Grade B")
+    }
+    else if(grade >= 70){
+        println("Grade C")
+    }
+    else{
+        println("Grade D")
+    }
+    
+   
+}
