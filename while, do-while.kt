b61a035266b95f0ec6b30ee11f027f177ba2721a
Shahana T_12219504 <119865026+ShahanaT12219504@users.noreply.github.com>
2024-08-27 13:52:57 +0530

    Create while, do-while.kt

---
+++ while, do-while.kt
@@ -0,0 +1,44 @@
+fun main() {
+    var i = 5
+    while(i>0){
+        println(i)
+        i--
+    }
+}
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>5)
+}
+
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>0)
+}
+
+fun main() {
+    var i = 5
+    do{
+        println("value is: $i")
+        i--
+    }
+    while(i>=0)
+}
+
+fun main() {
+    var i = 1
+    do{
+        println("value is: $i")
+        i++
+    }
+    while(i<=5)
+}
