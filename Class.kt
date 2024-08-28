f4f441d6d34b9c44e92c7173fdb1c6e9e47c50bd
Shahana T_12219504 <119865026+ShahanaT12219504@users.noreply.github.com>
2024-08-28 15:20:27 +0530

    Update Class.kt

--- Class.kt
+++ Class.kt
@@ -13,3 +13,14 @@ fun main(){
     val msg=ABC()
     msg.display()
 }
+
+
+class Student{
+    fun result(){
+        println("Pending")
+    }
+}
+fun main(){
+    val s1 = Student()
+    s1.result()
+}
