6138d6c25f679ed2e69c9fe023d0a41ef86a930b
Shahana T_12219504 <119865026+ShahanaT12219504@users.noreply.github.com>
2024-10-15 17:11:36 +0530

    Create Login page.kt

---
+++ Android Studio/Login page.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val Text = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+        val welcomeTextView = findViewById<TextView>(R.id.welcomeText)
+
+        loginButton.setOnClickListener {
+            Toast.makeText(this, "Welcome back", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
